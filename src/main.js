3fbf4b1776af202aff47eb8aca8ddadf6e63429a
Nazmul Islam <nazmulislamsami104@gmail.com>
2025-09-23 22:01:59 +0600

    Add some js. and min js. file

--- src/main.js
+++ src/main.js
@@ -21,3 +21,16 @@ window.addEventListener("mousemove", (e) => {
 		}px)`;
 	}
 });
+
+const scrollUp = () => {
+	const scrollUpBtn = document.getElementById("scroll-up");
+
+	if (scrollY >= 250) {
+		scrollUpBtn.classList.remove("-bottom-1/2");
+		scrollUpBtn.classList.add("bottom-4");
+	} else {
+		scrollUpBtn.classList.add("-bottom-1/2");
+		scrollUpBtn.classList.remove("bottom-4");
+	}
+};
+window.addEventListener("scroll", scrollUp);
